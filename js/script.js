89efa7a5c41fb33f89d034932c50ed1a59a0dfc7
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-09-18 19:37:40 +0200

    complete exercise

--- js/script.js
+++ js/script.js
@@ -11,8 +11,27 @@ tripsCounter.addEventListener("click", (event) => {
 const hotBadge = () => {
   const summerTrips = document.querySelectorAll("#collapseWelcomeSummer .row .card");
   summerTrips.forEach((trip) => {
+    trip.classList.add("position-relative");
     const badge = document.createElement("span");
+    badge.innerText = "HOT";
     badge.classList.add("badge");
-    trip.style.position = "relative";
+    badge.classList.add("position-absolute");
+    badge.classList.add("text-bg-danger");
+    badge.classList.add("top-0");
+    badge.style.translate = "-30% 50%";
+    badge.classList.add("end-0");
+    trip.appendChild(badge);
+    console.log(trip);
   });
 };
+
+window.addEventListener("load", hotBadge());
+
+const removeCardsBtn = document.getElementById("removeCards");
+
+const removeCards = () => {
+  const cards = Array.from(document.getElementsByClassName("card"));
+  cards.forEach((card) => card.classList.toggle("d-none"));
+};
+
+removeCardsBtn.addEventListener("click", removeCards);
